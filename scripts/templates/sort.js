84a7af09b4a2d8a7031aba877e131f67fd8ae4f4
Alexia Skrz <alexia.sireuille@gmail.com>
2023-02-23 23:27:15 +0100

    sort update

--- scripts/templates/sort.js
+++ scripts/templates/sort.js
@@ -26,19 +26,19 @@ function getSortChoice ( data )
 
   sortTypes = [
     {
-      name: "ingredients",
+      type: "ingredients",
       placeholder : "ingrédient",
       class : "ingredients",
       items: allIngredients,
     },
     {
-      name: "appareils",
+      type: "appareils",
       placeholder : "appareil",
       class : "appliances",
       items: allAppliances,
     },
     {
-      name: "ustensils",
+      type: "ustensils",
       placeholder : "ustensil",
       class : "tools",
       items: allTools,
@@ -47,8 +47,8 @@ function getSortChoice ( data )
 
   // construction du DOM ----------------
   const sortList = document.querySelector( ".sort__list" )
-// TODO améliorer le dropdown en construisant le sort__choice seulement à l'ouverture au lieu de jouer avec les class
-  function sortDropdown ( typeOfSorting )
+
+  function getSortDropdown ( typeOfSorting ) 
   {
     const sortItem = document.createElement( "li" )
     sortItem.className = `sort__item sort__item--close sort__item--${typeOfSorting.class}`
@@ -57,7 +57,7 @@ function getSortChoice ( data )
     const sortButton = document.createElement( "button" )
     sortButton.className = `sort__button sort__button--${typeOfSorting.class}`
     sortButton.type = "button"
-    sortButton.textContent = typeOfSorting.name
+    sortButton.textContent = typeOfSorting.type
     sortItem.appendChild( sortButton )
     
     const sortArrow = document.createElement( "img" )
@@ -65,50 +65,56 @@ function getSortChoice ( data )
     sortArrow.src = "../../assets/arrow-down.svg"
     sortArrow.alt = "arrow"
     sortButton.appendChild( sortArrow )
+  }
+
+  function getSortForm ( typeOfSorting, sortItem, currentTexte )
+  {
+    const sorType = (element) => element.type===currentTexte
+    const currentSort = typeOfSorting.findIndex( sorType )
     
     const sortForm = document.createElement( "form" )
-    sortForm.className = `sort__form sort__form--${typeOfSorting.class}`
+    sortForm.className = `sort__form sort__form--${typeOfSorting[currentSort].class}`
     sortItem.appendChild( sortForm )
     
     const sortWrapper = document.createElement( "div" )
-    sortWrapper.className = `sort__wrapper sort__wrapper--${typeOfSorting.class}`
+    sortWrapper.className = `sort__wrapper sort__wrapper--${typeOfSorting[currentSort].class}`
     sortForm.appendChild( sortWrapper
     )
 
     const sortField = document.createElement( "fieldset" )
-    sortField.className = `sort__field sort__field--${typeOfSorting.class}`
+    sortField.className = `sort__field sort__field--${typeOfSorting[currentSort].class}`
     sortWrapper.appendChild( sortField )
     
     const sortLabel = document.createElement( "label" )
     sortLabel.className = "sort__label"
-    sortLabel.setAttribute( "for", `sort__inputText--${typeOfSorting.class}` )
+    sortLabel.setAttribute( "for", `sort__inputText--${typeOfSorting[currentSort].class}` )
     sortField.appendChild( sortLabel )
 
     const sortInputText = document.createElement( "input" )
-    sortInputText.id = `sort__inputText--${typeOfSorting.class}`
-    sortInputText.className = `sort__inputText sort__inputText--${typeOfSorting.class}`
-    sortInputText.name = `sort__inputText--${typeOfSorting.class}`
+    sortInputText.id = `sort__inputText--${typeOfSorting[currentSort].class}`
+    sortInputText.className = `sort__inputText sort__inputText--${typeOfSorting[currentSort].class}`
+    sortInputText.name = `sort__inputText--${typeOfSorting[currentSort].class}`
     sortInputText.type = "text"
-    sortInputText.placeholder = `Rechercher un ${typeOfSorting.placeholder}`
+    sortInputText.placeholder = `Rechercher un ${typeOfSorting[currentSort].placeholder}`
     sortField.appendChild( sortInputText
     )
 
     const formButton = document.createElement( "button" )
-    formButton.className = `form__button form__button${typeOfSorting.class}`
+    formButton.className = `form__button form__button--${typeOfSorting[currentSort].class}`
     formButton.type = "button"
     sortWrapper.appendChild( formButton )
     
     const formArrow = document.createElement( "img" )
-    formArrow.className = `sort__arrow sort__arrow--up sort__arrow${typeOfSorting.class}`
+    formArrow.className = `sort__arrow sort__arrow--up sort__arrow${typeOfSorting[currentSort].class}`
     formArrow.src = "../../assets/arrow-down.svg"
     formArrow.alt = "arrow"
     formButton.appendChild( formArrow )
     
     const sortChoice = document.createElement( "div" )
-    sortChoice.className = `sort__choice sort__choice${typeOfSorting.class}`
+    sortChoice.className = `sort__choice sort__choice${ typeOfSorting[ currentSort ].class }`
+    // TODO ajouter le style pour le lignes du display grid
     sortForm.appendChild( sortChoice )
-    
-    typeOfSorting.items.forEach( item =>
+    typeOfSorting[currentSort].items.forEach( item =>
     {
       var choiceValue = document.createElement( "p" )
       choiceValue.className = "choice__value"
@@ -119,40 +125,52 @@ function getSortChoice ( data )
 
   sortTypes.forEach( sortType =>
   {
-    sortDropdown (sortType)
+    getSortDropdown( sortType )
   } )
   
   //événements-----------
- // TODO fermer le dropdown ouvert si un autre est ouvert
-  sortButtons = document.querySelectorAll( ".sort__button" )
+
+
+  const toObserve = document.querySelector( ".sort__list" )
+  // Création d'un observateur pour le changement du DOM
+  const observer = new MutationObserver( () =>
+  {
+    const formButtons = document.querySelectorAll( ".form__button" )
+    formButtons.forEach( formButton =>
+    {
+      const sortItem = formButton.closest( '.sort__item' )
+      function closeFormSort ()
+      {
+        if ( sortItem.classList.contains( "sort__item--open" ) )
+        {
+          sortItem.classList.remove( "sort__item--open" )
+          sortItem.classList.add( "sort__item--close" )
+          const sortForm = formButton.closest( ".sort__form" )
+          sortForm.remove()
+        }
+      }
+      formButton.addEventListener( "click", closeFormSort )
+    } )
+  } )
+  observer.observe( toObserve, { subtree: true, childList: true } )
+
+  // TODO fermer le dropdown ouvert si un autre est ouvert
+
+  var sortButtons = document.querySelectorAll( ".sort__button" )
   sortButtons.forEach( sortButton => 
   {
+    const currentTexte = sortButton.textContent
     const sortItem = sortButton.parentElement
-    function handleSortButton (  )
+    function OpenFormSort (  )
     {
       if ( sortItem.classList.contains( "sort__item--close" ) )
       {
         sortItem.classList.remove( "sort__item--close" )
-        sortItem.classList.add("sort__item--open")
+        sortItem.classList.add( "sort__item--open" )
+        getSortForm( sortTypes, sortItem, currentTexte )
+        formButton = document.querySelector( ".form__button" )
       }
     }
-    sortButton.addEventListener( "click", handleSortButton )
+    sortButton.addEventListener( "click", OpenFormSort )
   } )
-
-  formButtons = document.querySelectorAll( ".form__button" )
-  formButtons.forEach( formButton => 
-  {
-    const sortItem = formButton.closest('.sort__item')
-    function handleFormButton ()
-    {
-      
-      if ( sortItem.classList.contains( "sort__item--open" ) )
-      {
-        sortItem.classList.remove( "sort__item--open" )
-        sortItem.classList.add("sort__item--close")
-      }
-    }
-    formButton.addEventListener( "click", handleFormButton )
-  } 
-  )
 }
