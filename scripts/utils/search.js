faa236796bc7176fb9be55b26023e3390eee60e3
Alexia Skrz <alexia.sireuille@gmail.com>
2023-02-27 18:34:17 +0100

    functional search bar

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -11,9 +11,12 @@ searchForm.addEventListener( "submit", search )
 
 function search ()
 {
-  //TODO minimum 3 caractères
-  var recipesSearched = []
   const searchValue = SearchInput.value 
+  console.log( "searchValue : ", searchValue ) //*
+  const searchValueTrim = searchValue.trim()
+  console.log ("searchValueTrim.length : ",searchValueTrim.length) //*
+  if (searchValueTrim.length>2 || searchValueTrim.length===0){
+    var recipesSearched = []
   for ( const recipe of recipes )
   {
     var recipeSearched = []
@@ -26,21 +29,30 @@ function search ()
     }
     const stringRecipe = recipeSearched.join( " " )
     const stringRecipeLowerCase = stringRecipe.toLowerCase()
-    const searchValueToLowerCase = searchValue.toLowerCase()
-    const match = stringRecipeLowerCase.includes( searchValueToLowerCase )
+    const searchValueTrimToLowerCase = searchValueTrim.toLowerCase()
+    const match = stringRecipeLowerCase.includes( searchValueTrimToLowerCase )
     if ( match )
     {
       recipesSearched.push(recipe)
     }
   }
   console.log( "recipesSearched : ", recipesSearched ) //*
+  const recipesList = document.querySelector( ".recipes__list" )
   if ( recipesSearched.length > 0 )
   {
-  const recipesList = document.querySelector( ".recipes__list" )
   recipesList.innerHTML = ""
   recipesSearched.forEach( recipe => 
   getRecipe(recipe)
     )
+  }
+  else
+  {
+    recipesList.innerHTML = "<div class='notFound'> Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc </div>"
+    }
+  }
+  else
+  {
+    console.log ("au moins 3 caractères")
   }
 }
 
