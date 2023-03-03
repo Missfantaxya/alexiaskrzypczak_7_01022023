428569ccb06844220177593995b27b23856b4bf8
Alexia Skrz <alexia.sireuille@gmail.com>
2023-03-03 16:53:49 +0100

    FIX advancedSearch WIP

--- scripts/utils/advancedSearch.js
+++ scripts/utils/advancedSearch.js
@@ -1,62 +1,78 @@
+var recipesAdvancedSearched = []
+var tags = []
+var newRecipes = []
+console.log ("newRecipesSearch 1 : ", newRecipesSearch)
+
+// FIXME fonctionne que pour le 1er tag avec ou sans recherche
+
+// FIXME remet après un premier tri les 50 recettes au 2ème tag avec ou sans recherche
+
 function advancedSearch ()
 {
-  // console.log("recipes dans advancedSearch : ", recipes) //*
-    console.log ("newRecipesSearch dans adsancedSearch : ", newRecipesSearch) 
-  
+  newRecipes = ( newRecipesSearch.length > 0 ? newRecipesSearch : recipes )
+  console.log ("newRecipes 1 : ",newRecipes)
   const tagsCards = document.querySelectorAll( ".tag__item" )
-
-  var tags = []
+  console.log ("newRecipesSearch 2 : ", newRecipesSearch)
   tagsCards.forEach( tag =>
   {
-    tags.push(tag.textContent)
+    const tagText =  tag.textContent
+    tags.push( tagText )
   } )
-  if ( tags.length > 0 )
+  const newSetTags = new Set( tags )
+  const allTags = [ ...newSetTags ]
+  console.log ("allTags : ", allTags) //*
+  if ( allTags.length === 1 )
   {
-    const stringTags = tags.join( " " )
-    // s'il n'y a pas eu de recherche 
-    var recipesAdvancedSearched = []
-    const search = newRecipesSearch.length > 0
-    console.log( "search : ", search ) //*
+    const stringTags = allTags.join( " " )
+    console.log( "stringTags : ", stringTags )//*
+    console.log("newRecipes 2 : ", newRecipes)
+    console.log("newRecipesSearch : ", newRecipesSearch) //*
     
     function sortRecipes (recipesToSort)
     {
       recipesToSort.forEach( recipe =>
       {
-        var recipeSearched = []
-        recipeSearched.push( recipe.appliance )
+        var inRecipeSearched = []
+        inRecipeSearched.push( recipe.appliance )
         recipe.ingredients.forEach( ingredient =>
         {
-          recipeSearched.push(ingredient.ingredient)
+          inRecipeSearched.push(ingredient.ingredient)
         } )
         recipe.ustensils.forEach( ustensil =>
         {
-          recipeSearched.push(ustensil)
+          inRecipeSearched.push(ustensil)
         } )
-        const stringRecipe = recipeSearched.join( " " )
+        const stringRecipe = inRecipeSearched.join( " " )
         const stringRecipeLowerCase = stringRecipe.toLowerCase()
         const match = stringRecipeLowerCase.includes( stringTags )
         if ( match )
         {
-          recipesAdvancedSearched.push(recipe)
+          recipesAdvancedSearched.push( recipe )
+          const newSetRecipes = new Set( recipesAdvancedSearched )
+          newRecipes = [ ...newSetRecipes ]
         }
-        const recipesList = document.querySelector( ".recipes__list" )
-        recipesList.innerHTML = " "
-        recipesAdvancedSearched.forEach( recipe =>
-        {
-          getRecipe(recipe)
-        } )
+      } )
+      // const newSetRecipes = new Set( recipesAdvancedSearched )
+      // newRecipes = [ ...newSetRecipes ]
+      console.log("newRecipes 3 : ", newRecipes)
+      const recipesList = document.querySelector( ".recipes__list" )
+      recipesList.innerHTML = " "
+      newRecipes.forEach( recipe =>
+      {
+        getRecipe(recipe)
       } )
     }
+    const search = newRecipesSearch.length > 0
+    console.log( "search : ", search ) //*
 
-    if (!search)
+    if ( !search )
     {
-      sortRecipes (recipes)
+      sortRecipes(newRecipes)
     }
     else
     {
-      sortRecipes(newRecipesSearch)
+      sortRecipes( newRecipesSearch )
     }
-
   }
   else
   {
